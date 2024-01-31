ff57edc3349edb8848e3497ef9e3dde19cc5d15c
rizlainezaroual <rizlainezar@gmail.com>
2024-01-31 19:12:30 +0100

    Corrections

--- Code/header/playermedium.hpp
+++ Code/header/playermedium.hpp
@@ -9,7 +9,7 @@ class PlayerMedium : public Player
     public: 
         PlayerMedium(std::string lastname, std::string firstname, float Hp,float Hp_max,float Hp_multiplier, float x, float y, float damage, std::map<std::string, int> textureAssociations, std::string deathTexturePath);
         ~PlayerMedium() {};
-        void doAttack1(Player& targetPlayer); //
+        void doAttack1(Player& targetPlayer); 
         void randomAttack(Player& targetPlayer);
         bool canAttack() const;
         PlayerMedium& operator=(const PlayerMedium& other);
